a9fb597483dec79f170f1e57606805e9c737b725
Amar A <amaratasia@gmail.com>
2017-11-16 00:53:21 +0530

    Remove Raise

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
 	def signin
 		if (params[:email].present? && params[:password].present?)
   			user = User.find_by_email(params[:email])
-  			# return render json: {}, status: 401 if user.blank?
-  			raise 'amar'
+  			return render json: {}, status: 401 if user.blank?
+  			# raise 'amar'
   			if user.valid_password? params[:password]
 	  			sign_in user
 	  			return render json: {token: user.regenerate_token, user: user}, donot_track: true  
